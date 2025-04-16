03c73b0ea841610ac058c7f6b5af2a26987597e5
Arthur16800 <arthur08coutinho@gmail.com>
2025-04-16 09:16:18 -0300

    Preparação sprint2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,69 @@
 import * as React from "react";
+import Button from "@mui/material/Button";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import GroupsIcon from "@mui/icons-material/Groups";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        gap: "70px",
-        marginTop: "140px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#D9D9D9",
-          border: "none",
-          borderRadius: "10px",
-          fontSize: "22px",
-          width: "200px",
-          height: "150px",
-        }}
+    <>
+      <Button
+        sx={{ display: "flex", marginLeft: "93%", marginTop: 0,}}
+        // onClick={() => navigate("/perfil")}
       >
-       
-        Salas
-      </div>
+        <AccountCircleIcon sx={{ fontSize: 40, color:'#807F7F' }} />
+      </Button>
 
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
           justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#D9D9D9",
-          border: "none",
-          borderRadius: "10px",
-          fontSize: "22px",
-          width: "200px",
-          height: "150px",
+          gap: "70px",
+          marginTop: "140px",
         }}
       >
-        Reservas
+        <Button
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#D9D9D9",
+            border: "none",
+            borderRadius: "10px",
+            fontSize: "20px",
+            width: "200px",
+            height: "150px",
+            color: "black",
+          }}
+          onClick={() => navigate("/listaSalas")}
+        >
+          <GroupsIcon sx={{ fontSize: 80 }} />
+          Salas
+        </Button>
+
+        <Button
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#D9D9D9",
+            border: "none",
+            borderRadius: "10px",
+            fontSize: "20px",
+            width: "200px",
+            height: "150px",
+            color: "black",
+          }}
+        >
+          <EventAvailableIcon sx={{ fontSize: 70 }} />
+          Reservas
+        </Button>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/ListaSalas.jsx
+++ src/pages/ListaSalas.jsx
@@ -8,6 +8,8 @@ import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import api from "../axios/axios";
 import { useNavigate } from "react-router-dom";
+import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
+import Button from "@mui/material/Button";
 
 function listRooms() {
   const [rooms, setRooms] = useState([]);
@@ -28,8 +30,10 @@ function listRooms() {
 
   const listRooms = rooms.map((sala) => {
     return (
-      <TableRow sx={{ borderBottom: "2px solid #fff"}} key={sala.number}>
-        <TableCell sx={{ }} align="center">{sala.number}</TableCell>
+      <TableRow sx={{ borderBottom: "2px solid #fff" }} key={sala.number}>
+        <TableCell sx={{}} align="center">
+          {sala.number}
+        </TableCell>
         <TableCell align="center">{sala.description}</TableCell>
         <TableCell align="center">{sala.capacity}</TableCell>
       </TableRow>
@@ -41,42 +45,60 @@ function listRooms() {
   }, []);
 
   return (
-    <div>
-      {rooms.length === 0 ? (
-        <h1>Carregando Salas</h1>
-      ) : (
-        <div >
-          <h2 style={{display:"flex", justifyContent:"center", color:"#807F7F"}}>Salas</h2>
-          <TableContainer component={Paper} style={{ margin: "14px" }}>
-            <Table size="small">
-              <TableHead
-                style={{
-                  backgroundColor: "#D9D9D9",
-                  border: "10px solid white none",
-                }}
-              >
-                <TableRow>
-                  <TableCell align="center">Número</TableCell>
-                  <TableCell align="center">Descrição</TableCell>
-                  <TableCell align="center">Capacidade</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody
-                sx={{
-                  backgroundColor: "#E9E7E7",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding:"100px",
-                }}
-              >
-                {listRooms}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          
-        </div>
-      )}
-    </div>
+    <>
+       <Button
+        sx={{ display: "flex", marginLeft: "93%", marginTop: 0,}}
+        onClick={() => navigate("/home")}
+      >
+        <ArrowCircleLeftOutlinedIcon sx={{ fontSize: 40, color:'#807F7F' }} />
+      </Button>
+
+
+    
+      <div>
+        {rooms.length === 0 ? (
+          <h1>Carregando Salas</h1>
+        ) : (
+          <div>
+            <h2
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                color: "#807F7F",
+              }}
+            >
+              Salas
+            </h2>
+            <TableContainer component={Paper} style={{ margin: "14px" }}>
+              <Table size="small">
+                <TableHead
+                  style={{
+                    backgroundColor: "#D9D9D9",
+                    border: "10px solid white none",
+                  }}
+                >
+                  <TableRow>
+                    <TableCell align="center">Número</TableCell>
+                    <TableCell align="center">Descrição</TableCell>
+                    <TableCell align="center">Capacidade</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody
+                  sx={{
+                    backgroundColor: "#E9E7E7",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "100px",
+                  }}
+                >
+                  {listRooms}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 export default listRooms;
